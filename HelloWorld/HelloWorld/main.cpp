271222d9837172f3edf44060fbf9f92837811064
Jude <jude@Mikes-iMac.local>
2018-05-05 11:31:08 -0700

    Add main.cpp

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Jude on 5/5/18.
+//  Copyright © 2018 Jude. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+string hello();
+
+int main(int argc, const char * argv[]) {
+    // ask for your name
+    cout << "put in your name ";
+    string name;
+    cin >> name;
+    cout << "Nice to meet you " << name << "\n";
+    return 0;
+}
+
+string hello(string name) {
+    return "";
+    
+}
